43a8aa2b4779c251c4211f951c5c01bda5d408e5
Ana <anam.rodriguez13@gmail.com>
2026-08-16 23:33:55 -0500

    fix: inforce base path

--- svelte.config.ts
+++ svelte.config.ts
@@ -4,14 +4,19 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
+	compilerOptions: {
+		runes: true
+	},
 	kit: {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
 			fallback: 'index.html'
 		}),
+		appDir: 'app',
 		paths: {
-			base: '/encuesta-distrital-percepcion' // THIS IS THE KEY FIX
+			base: '/encuesta-distrital-percepcion',
+			relative: false
 		}
 	}
 };
